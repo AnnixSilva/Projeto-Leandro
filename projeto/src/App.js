43e5da6f190a10b89bda3d5001a9c9553fc80910
ANNIELE SILVA DOS SANTOS <021.898683@senaicimatec.edu.br>
2024-04-19 17:14:43 -0300

    att 19/04

--- projeto/src/App.js
+++ projeto/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Home from './Componentes/Home';
+import Home from './Home.js';
 import { ReactComponent as Logo } from './logo.svg';
 import { ReactComponent as Cart } from './cart.svg';
 import { ReactComponent as Logo2 } from './logo2.svg';
 import { ReactComponent as Logo3 } from './logo3.svg';
-import { ReactComponent as Logo4 } from './logo4.svg';
-import { ReactComponent as Logo5 } from './logo5.svg';
+import { ReactComponent as Placa1 } from './placa1.svg';
+import { ReactComponent as Placa2 } from './placa2.svg';
 import { ReactComponent as Ifood } from './ifood.svg';
 import { ReactComponent as Delivery } from './delivery.svg';
-import { ReactComponent as Logo7 } from './logo7.svg';
+import { ReactComponent as Placa3 } from './placa3.svg';
+import Card from './card.js';
+
    
 
 function App() {
@@ -20,14 +22,12 @@ function App() {
         <Cart className="App-Cart"/>
         <Logo2 className="App-Logo2"/>
         <Logo3 className="App-Logo3"/>
-       <div className='App-Logo4'>
-       <Logo4/>
-       <Logo5/>
-       <Logo7
-       />
-       </div>
-      <Ifood className="ifood"/>
-      <Delivery className="Delivery"/>
+        <Placa1 className= "App-Placa1"/>
+        <Placa2 className= "App-Placa2"/>
+        <Ifood className= "Ifood"/>
+        <Delivery className= "delivery"/>
+        <Placa3 className= "App-Placa3"/>
+        <Card/>
       </header>
     </div>
   );
---
+++ projeto/src/card.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function Card(){
+    return(
+        <div className="card">
+            <img src="https://cdn-icons-png.flaticon.com/512/1046/1046849.png" alt=''/>
+        </div>
+    )
+}
+
+export default Card;
